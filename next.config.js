044524cfe2bf84ee12c3dae282b149450ce0a974
felipe gomez <ggfelipegc@gmail.com>
2023-07-29 19:10:09 -0500

    build add sass preprocessor

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+  },
+  ...nextConfig,
+};
