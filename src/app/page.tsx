8982299c85f048c921a0d54e0696a4a373efa771
felipe gomez <ggfelipegc@gmail.com>
2023-08-07 13:16:59 -0500

    fix: implement lint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.scss'
+import Image from 'next/image';
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
@@ -91,5 +91,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
