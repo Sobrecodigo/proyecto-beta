e0a213c093529994a28e56b8566d58b7224c7831
felipe gomez <ggfelipegc@gmail.com>
2023-09-07 16:23:34 -0500

    feat: add input types constants

---
+++ src/constants/inputTypes.ts
@@ -0,0 +1,6 @@
+export const TEXT = 'text';
+export const PASSWORD = 'password';
+export const EMAIL = 'email';
+export const CHECKBOX = 'checkbox';
+export const RADIO = 'radio';
+export const NUMBER = 'number';
